d6578e4e44fba2c0a4b5900732fc3e7582615824
trentonsweat <trentonsweat@gmail.com>
2017-09-27 16:44:39 -0400

    LabNumber3

---
+++ LabNumber3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ProgramContinue = true;
+
+            while (ProgramContinue == true)
+            {
+                // declare variables
+                int input;
+                Console.WriteLine("Learn Your squares and cubes!");
+                Console.WriteLine("Enter an integer");
+                input = int.Parse(Console.ReadLine());
+                Console.WriteLine("Number" + "\t" + "Squared" + "\t" + "Cubed");
+
+                //process
+
+                for (int i = 1; i <= input; i++)
+                {
+                    Console.WriteLine(i + "\t" + (i + i) + "\t" +(i + i + i));
+                }
+
+
+                string UserChoice;
+                Console.WriteLine("Do you want to run the code again? (N/No/y/yes)");
+                UserChoice = Console.ReadLine();
+                if (UserChoice == "N" || UserChoice == "No")
+                {
+                    ProgramContinue = false;
+                }
+            }
+
+
+
+        }
+    }
+}
+
+
